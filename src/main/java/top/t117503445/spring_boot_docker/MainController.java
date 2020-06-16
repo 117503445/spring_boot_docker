c7e130d3d78c6ef2280e2aa420a34c559c38b0f5
117503445 <t117503445@gmail.com>
2020-06-16 12:17:05 +0800

    add maven version

--- src/main/java/top/t117503445/spring_boot_docker/MainController.java
+++ src/main/java/top/t117503445/spring_boot_docker/MainController.java
@@ -1,13 +1,28 @@
 package top.t117503445.spring_boot_docker;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    /**
+     * 当前版本
+     */
+    @Value("${app.version}")
+    private String version;
+    /**
+     * 打包时间
+     */
+    @Value("${app.build.time}")
+    private String buildTime;
+
     @GetMapping
-    public String Hello(){
-        return "This is Spring Boot Docker Test :D";
+    public String Hello() {
+        System.out.println(version);
+        System.out.println(buildTime);
+        return String.format("This is Spring Boot Docker Test :D {%s} {%s}", version, buildTime);
+
     }
 }
